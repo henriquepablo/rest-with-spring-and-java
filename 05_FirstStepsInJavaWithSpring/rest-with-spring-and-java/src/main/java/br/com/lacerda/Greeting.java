cc962486461c2eb15a1db078079e7f3009b0247b
henriquepablo <pablo269704@gmail.com>
2024-03-16 12:41:39 -0300

    init: criando projeto

---
+++ 05_FirstStepsInJavaWithSpring/rest-with-spring-and-java/src/main/java/br/com/lacerda/Greeting.java
@@ -0,0 +1,24 @@
+package br.com.lacerda;
+
+public class Greeting {
+
+	private final Long id;
+	private final String content;
+	
+	public Greeting(Long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+	
+	
+	
+	
+}
