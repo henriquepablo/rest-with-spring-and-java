cdb9f3bc30593769bbc9dd8f514780d6e5b9ac32
henriquepablo <pablo269704@gmail.com>
2024-03-17 14:40:20 -0300

    feat: using HTTP verbs: GET, POST, PUT, DELETE

--- 07_SpringDependencyInjection/rest-with-spring-and-java/src/main/java/br/com/lacerda/PersonController.java
+++ 07_SpringDependencyInjection/rest-with-spring-and-java/src/main/java/br/com/lacerda/PersonController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +29,23 @@ public class PersonController {
 	@RequestMapping(method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
 	public List<Person> findAll() {
 		return service.findAll();
+	}
+	
+	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE, method=RequestMethod.POST, produces=MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) throws Exception{
+		
+		return service.create(person);
+	}
+	
+	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE, method=RequestMethod.PUT, produces=MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) throws Exception{
+		
+		return service.update(person);
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public void delete(@PathVariable(value = "id") String id) throws Exception{
+		
+		service.delete(id);
 	}
 }
--- 07_SpringDependencyInjection/rest-with-spring-and-java/src/main/java/br/com/lacerda/services/PersonServices.java
+++ 07_SpringDependencyInjection/rest-with-spring-and-java/src/main/java/br/com/lacerda/services/PersonServices.java
@@ -16,16 +16,16 @@ public class PersonServices {
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
 	public List<Person> findAll() {
-		
+
 		logger.info("Find all persons");
-		
+
 		List<Person> persons = new ArrayList<>();
 
 		for (int i = 0; i < 8; i++) {
 			Person person = mockPerson(i);
 			persons.add(person);
 		}
-		
+
 		return persons;
 	}
 
@@ -42,6 +42,26 @@ public class PersonServices {
 		return person;
 	}
 
+	public Person create(Person person) {
+
+		logger.info("Create a person");
+
+		return person;
+	}
+
+	public Person update(Person person) {
+
+		logger.info("Update a person");
+
+		return person;
+	}
+	
+	public void delete(String id) {
+		
+		logger.info("Delete a person");
+		
+	}
+
 	private Person mockPerson(int i) {
 
 		Person person = new Person();
@@ -51,7 +71,7 @@ public class PersonServices {
 		person.setLastName("Last name " + i);
 		person.setAddres("Some addres in Brasil " + i);
 		person.setGender("Male");
-		
+
 		return person;
 	}
 }
