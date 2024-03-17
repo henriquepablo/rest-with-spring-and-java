48ae871f639f91dc76785f8bdb98b9543a61d6dc
henriquepablo <pablo269704@gmail.com>
2024-03-17 13:48:39 -0300

    feat: implement findAll

--- 07_SpringDependencyInjection/rest-with-spring-and-java/src/main/java/br/com/lacerda/PersonController.java
+++ 07_SpringDependencyInjection/rest-with-spring-and-java/src/main/java/br/com/lacerda/PersonController.java
@@ -1,5 +1,7 @@
 package br.com.lacerda;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,5 +23,10 @@ public class PersonController {
 	public Person findById(@PathVariable(value="id") String id) throws Exception{
 		
 		return service.findById(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+		return service.findAll();
 	}
 }
--- 07_SpringDependencyInjection/rest-with-spring-and-java/src/main/java/br/com/lacerda/services/PersonServices.java
+++ 07_SpringDependencyInjection/rest-with-spring-and-java/src/main/java/br/com/lacerda/services/PersonServices.java
@@ -1,5 +1,7 @@
 package br.com.lacerda.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -9,20 +11,47 @@ import br.com.lacerda.model.Person;
 
 @Service
 public class PersonServices {
-	
+
 	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
-	public Person findById(String id) {
+	public List<Person> findAll() {
 		
+		logger.info("Find all persons");
+		
+		List<Person> persons = new ArrayList<>();
+
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		
+		return persons;
+	}
+
+	public Person findById(String id) {
+
 		logger.info("Finding one person");
 		Person person = new Person();
-		
+
 		person.setId(counter.incrementAndGet());
 		person.setFirstName("Pablo");
 		person.setLastName("Lacerda");
 		person.setAddres("Parelheiros - São Paulo - Brasil");
 		person.setGender("Male");
+		return person;
+	}
+
+	private Person mockPerson(int i) {
+
+		Person person = new Person();
+
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLastName("Last name " + i);
+		person.setAddres("Some addres in Brasil " + i);
+		person.setGender("Male");
+		
 		return person;
 	}
 }
