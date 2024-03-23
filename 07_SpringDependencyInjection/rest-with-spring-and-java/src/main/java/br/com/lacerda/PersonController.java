f42fed9cc9ba4efc3d0faf99ce8dfb1931f45ef8
henriquepablo <pablo269704@gmail.com>
2024-03-23 10:31:01 -0300

    refactor: Refactoring the Person Controller Code

--- 07_SpringDependencyInjection/rest-with-spring-and-java/src/main/java/br/com/lacerda/PersonController.java
+++ 07_SpringDependencyInjection/rest-with-spring-and-java/src/main/java/br/com/lacerda/PersonController.java
@@ -4,11 +4,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.DeleteExchange;
 
 import br.com.lacerda.model.Person;
 import br.com.lacerda.services.PersonServices;
@@ -20,32 +26,34 @@ public class PersonController {
 	@Autowired
 	private PersonServices service;
 	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value="/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
 	public Person findById(@PathVariable(value="id") Long id) throws Exception{
 		
 		return service.findById(id);
 	}
 	
-	@RequestMapping(method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(produces=MediaType.APPLICATION_JSON_VALUE)
 	public List<Person> findAll() {
 		return service.findAll();
 	}
 	
-	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE, method=RequestMethod.POST, produces=MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
 	public Person create(@RequestBody Person person) throws Exception{
 		
 		return service.create(person);
 	}
 	
-	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE, method=RequestMethod.PUT, produces=MediaType.APPLICATION_JSON_VALUE)
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
 	public Person update(@RequestBody Person person) throws Exception{
 		
 		return service.update(person);
 	}
 	
-	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
-	public void delete(@PathVariable(value = "id") Long id) throws Exception{
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) throws Exception{
 		
 		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
