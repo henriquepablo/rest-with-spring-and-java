7e90e009e6ce83f4795f7066a2f9891aff952b64
henriquepablo <pablo269704@gmail.com>
2024-03-29 08:17:50 -0300

    feat: customizando o retorno do json

---
+++ 12_CustomJSON/rest-with-spring-and-java/src/main/resources/db/migrations/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUE
+  (1, 'Ayrton', 'Senna', 'São Paulo', 'Male'),
+  (2, 'Leonardo', 'da Vinci', 'Anchiano', 'Male'),
+  (4, 'Indira', 'Gadhi', 'Porbandar - India','Female'),
+  (5, 'Mahatma', 'Gadhi', 'Porbandar - India', 'Male'),
+  (7, 'Muhammad', 'Ali', 'Kentucky - US', 'Male'),
+  (8, 'Zulai', 'diniz', 'Texas', 'Male'),
+  (9, 'Nikola', 'teslas', 'Jamaica', 'Male');
